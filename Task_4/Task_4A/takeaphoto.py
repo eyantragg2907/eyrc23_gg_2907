8ee9aa5b2c7f935d56067248c1a2b854cbfd9194
ThisAbhinav <this.abhinav26@gmail.com>
2023-12-24 13:16:05 +0530

    skipping frames

--- Task_4/Task_4A/takeaphoto.py
+++ Task_4/Task_4A/takeaphoto.py
@@ -2,15 +2,19 @@ CAMERA_ID = 1 # 0 for internal, 1 for external as a basis
 
 import cv2
 from datetime import datetime
-
+num_of_frames_skip = 100
 # Initialize the camera
 cap = cv2.VideoCapture(CAMERA_ID)
+if not cap.open(0):
+    print("CAMERA NOT OPEN. ERROR")
+else:
+    # take a photo
+    for i in range(num_of_frames_skip):
+        ret, frame = cap.read()
 
-# take a photo
-ret, frame = cap.read()
-
-# save the photo
-addr = f"temp_snap_{str(datetime.now().timestamp()).replace('.', '-')}.png"
-cv2.imwrite(addr, frame)
-cap.release()
+    # save the photo
+    if ret is True:
+        addr = f"temp_snap_{str(datetime.now().timestamp()).replace('.', '-')}.png"
+        cv2.imwrite(addr, frame)
+        cap.release()
 
