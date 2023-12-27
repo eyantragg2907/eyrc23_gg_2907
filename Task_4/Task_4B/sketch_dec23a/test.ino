4ba9d4954b9195b04ba862eb301107e732668650
ThisAbhinav <this.abhinav26@gmail.com>
2023-12-27 16:09:31 +0530

    testing files

---
+++ Task_4/Task_4B/sketch_dec23a/test.ino
@@ -0,0 +1,275 @@
+#include <WiFi.h>
+
+const char *ssid = "pjrWifi";          // Enter your wifi hotspot ssid
+const char *password = "SimplePass01"; // Enter your wifi hotspot password
+const uint16_t port = 8002;
+const char *host = "192.168.229.92";
+
+const int IR1 = 5; // IR sensors pins
+const int IR2 = 18;
+const int IR3 = 32;
+const int IR4 = 33;
+const int IR5 = 25;
+
+const int motor1f = 12; // motor LEFT
+const int motor1r = 14;
+
+const int motor2f = 13; // motor RIGHT
+const int motor2r = 27;
+
+const int led_red = 2; // misc
+const int led_green = 15;
+const int buzzer = 23;
+
+int speed1 = 255; // motor speeds
+int speed2 = 255;
+
+int turntime = 1000; // in milliseconds
+
+int input1, input2, input3, input4, input5; // inputs of IR sensors, returns LOW when black line
+
+bool mode = 0; // 0 is bang bang, 1 is the middle line
+
+// String path = "nnnrlrrnrnln"; // n means do nothing, l = left, r = right
+String path = "";
+
+char incomingPacket[80];
+WiFiClient client;
+
+String msg = "0";
+int counter = 0;
+
+int i = 0; // flags
+int move = false;
+bool atNode = true;
+void forward()
+{
+  Serial.println("MOVING FORWARD");
+  if (mode == 0)
+  { // bang bang
+    if (!input1)
+    { // left IR hits black
+      analogWrite(motor1f, speed1);
+    }
+    else if (!input5)
+    { // right IR hits black
+      analogWrite(motor2f, speed2);
+    }
+    else
+    {
+      analogWrite(motor1f, speed1);
+      analogWrite(motor2f, speed2);
+    }
+  }
+  else
+  { // middle black line
+    if (!input3)
+    { // following the line
+      analogWrite(motor1f, speed1);
+      analogWrite(motor2f, speed2);
+    }
+    if (!input4)
+    { // right IR touches black
+      analogWrite(motor1f, speed1);
+    }
+    if (!input2)
+    { // left IR touches black
+      analogWrite(motor2f, speed2);
+    }
+  }
+}
+void stop()
+{
+  analogWrite(motor1f, 0);
+  analogWrite(motor2f, 0);
+  analogWrite(motor1r, 0);
+  analogWrite(motor2r, 0);
+}
+void goToNextNode()
+{
+  atNode = false;
+  if (!(input2 || input3 || input4))
+  { // go ahead until middle IRs see black
+    Serial.println("black forward march");
+    forward();
+  }
+  else
+  {
+    stop();
+    Serial.println("NODE");
+    atNode = true;
+    digitalWrite(buzzer, LOW);
+    delay(1000);
+    digitalWrite(buzzer, HIGH);
+  }
+}
+
+// void turn(char x)
+// {
+//   stop();
+//   if (x == 'l')
+//   {
+//     analogWrite(motor2f, speed2);
+//     analogWrite(motor1r, speed1);
+//     }
+//   else
+//   {
+//     analogWrite(motor1f, speed1);
+//     analogWrite(motor2r, speed2);
+//   }
+//   delay(turntime);
+//   stop();
+// }
+void turn(char x)
+{
+  stop();
+  analogWrite(motor1f, speed1);
+  analogWrite(motor2f, speed2); //  move forward till we leave the node
+  // delay(200); // another logic is to move a little and then rotate till the next node.
+  // stop();
+  if (input2 && input4)
+  { // we leave the middle node when the middle left and middle right ir sensor is on gray
+    stop();
+  }
+  while (!input3) // rotate till it leaves the current black line
+  {
+    if (x == 'l')
+    {
+      analogWrite(motor1r, speed1);
+      analogWrite(motor2f, speed2);
+    }
+    else
+    {
+      analogWrite(motor1f, speed1);
+      analogWrite(motor2r, speed2);
+    }
+  }
+  // till now we have left the current black line
+  // now we will rotate will we find the next black line
+  if (x == 'l')
+  {
+    analogWrite(motor1r, speed1);
+    analogWrite(motor2f, speed2);
+  }
+  else
+  {
+    analogWrite(motor1f, speed1);
+    analogWrite(motor2r, speed2);
+  }
+  while (input3)
+  {
+    Serial.println("rotating till it finds the middle line again");
+  }
+  stop();
+}
+void setup()
+{
+
+  // set  modes
+  pinMode(IR1, INPUT);
+  pinMode(IR2, INPUT);
+  pinMode(IR3, INPUT);
+  pinMode(IR4, INPUT);
+  pinMode(IR5, INPUT);
+
+  pinMode(motor1f, OUTPUT);
+  pinMode(motor1r, OUTPUT);
+  pinMode(motor2f, OUTPUT);
+  pinMode(motor2r, OUTPUT);
+
+  pinMode(led_red, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+
+  digitalWrite(buzzer, HIGH);
+
+  Serial.begin(115200);
+
+  // setting up wifi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("...");
+  }
+
+  Serial.print("WiFi connected with IP: ");
+  Serial.println(WiFi.localIP());
+
+  digitalWrite(led_red, HIGH); // rest condition
+  digitalWrite(led_green, LOW);
+
+  do
+  {
+    Serial.println("Connection to host failed");
+    // digitalWrite(buzzer, LOW);
+    digitalWrite(led_red, HIGH);
+    delay(200);
+    // digitalWrite(buzzer, HIGH);
+  } while (!client.connect(host, port));
+
+  digitalWrite(led_red, LOW);
+  client.print("Obese American ate 69 giant ramen bowl but still is lighter than your mom"); // Send an acknowledgement to host(laptop)
+  msg = client.readStringUntil('\n');                                                        // Read the message through the socket until new line char(\n)
+  path = msg;
+  move = true;
+  Serial.println(path);
+}
+
+void loop()
+{
+  input1 = digitalRead(IR1); // read IR sensors
+  input2 = digitalRead(IR2);
+  input3 = digitalRead(IR3);
+  input4 = digitalRead(IR4);
+  input5 = digitalRead(IR5);
+
+  if (!(input2 && input3 && input4))
+  { // switch to correct mode
+    mode = 0;
+  }
+  else
+  {
+    mode = 1;
+  }
+  Serial.print("mode ");
+  Serial.println(mode);
+
+  if (move)
+  { // start moving
+    Serial.println("MOVING...");
+    digitalWrite(led_red, LOW);
+    digitalWrite(led_green, HIGH);
+    if (atNode)
+    {
+      Serial.print("At a node... ");
+      char command = path[i++];
+      Serial.println(command);
+      if (command == 'l')
+      {
+        turn(command);
+      }
+      if (command == 'r')
+      {
+        turn(command);
+      }
+      Serial.println("Moving ahead!");
+      analogWrite(motor1f, speed1);
+      analogWrite(motor2f, speed2); // pushing the robot out of the node
+    }
+    Serial.println("Go to next");
+    goToNextNode();
+
+    if (i == path.length())
+    {
+      Serial.println("path complete");
+      move = false; // break out of the loop
+      stop();
+      digitalWrite(led_green, LOW);
+      digitalWrite(led_red, HIGH);
+      digitalWrite(buzzer, LOW);
+      delay(5000);
+      digitalWrite(led_red, LOW);
+      digitalWrite(buzzer, HIGH);
+    }
+  }
+}
---
+++ Task_4/Task_4B/test.py
@@ -0,0 +1,68 @@
+"""
+*****************************************************************************************
+*
+*        		===============================================
+*           		Geo Guide (GG) Theme (eYRC 2023-24)
+*        		===============================================
+*
+*  This script is to implement Task 4A of Geo Guide (GG) Theme (eYRC 2023-24).
+*  
+*  This software is made available on an "AS IS WHERE IS BASIS".
+*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
+*  any and all claim(s) that emanate from the use of the Software or 
+*  breach of the terms of this agreement.
+*
+*****************************************************************************************
+"""
+
+# Team ID:			[ 2907 ]
+# Author List:		[ Arnav Rustagi, Abhinav Lodha]
+# Filename:			task_4b.py
+
+
+####################### IMPORT MODULES #######################
+
+import sys
+import time
+import pandas as pd
+import socket
+import signal		
+
+##############################################################
+
+ip = "192.168.229.92"     # Enter IP address of laptop after connecting it to WIFI hotspot
+commandsent = 0
+command = "nnnrlrrnrnln"
+################# ADD UTILITY FUNCTIONS HERE #################
+
+
+def cleanup(s):
+    s.close()
+    print("cleanup done")
+    sys.exit(0)
+
+    
+def send_to_robot():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((ip, 8002))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                print(data)
+                print(command)
+                conn.sendall(str.encode(str(command)))
+                time.sleep(1)
+                cleanup(s)
+    
+
+
+
+###############	Main Function	#################
+if __name__ == "__main__":
+    if commandsent == 0:
+        send_to_robot()
+        commandsent = 1
